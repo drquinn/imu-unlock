72f3a6e080b16c6eaad32ef12e9f4e2173305b36
drquinn <blong123@gmail.com>
2014-11-14 21:14:17 -0700

    clean up clientjs

--- public/js/client.js
+++ public/js/client.js
@@ -1,14 +1,5 @@
 var socket = io.connect('localhost:5000');
 
-socket.on('pong', function (data) {
-    console.log("pong");
-});
-
-socket.on('udp_update', function(data) {
-  console.log(data);
-  $("#log_data").append("<div>" + data + "</div>");
-});
-
 socket.on('serial_update', function(data) {
   console.log(data);
   $("#log_data").append("<div>" + data + "</div>");
@@ -16,12 +7,5 @@ socket.on('serial_update', function(data) {
 
 $.get('/getIP', function(res) {
     console.log(res);
-});
-
-
-$(document).ready(function() {
-    $("#hello").click(function(){
-        socket.emit('ping', { duration: 2 });
-    }); 
 });
 
--- public/threejs/unlockBase.js
+++ public/threejs/unlockBase.js
@@ -1,4 +1,4 @@
-
+'use strict';
 
 var unlock = function() {
     // private members
